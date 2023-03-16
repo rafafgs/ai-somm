06089b75b31e7cab612edcc09a37992eff2b0a00
Rafael Fernandes Gomes <95232805+rafafgs@users.noreply.github.com>
2023-03-16 16:40:06 +0000

    Add files via upload

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import openai
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/taste/', methods=['GET'])
+def process_form_get():
+        
+    process_form_post()
+    return 'Received form data successfully!'
+
+@app.route('/taste/', methods=['POST'])
+def process_form_post():
+    body = request.form['body']
+    alcohol = request.form['alcohol']
+    acidity = request.form['acidity']
+    tannins = request.form['tannins']
+    wine_type = request.form['wine-type']
+    flavors = request.form.getlist('flavors')
+    
+    result = f"I want you to give me a list of 3 possible wine styles that match these parameters. Acidity (1 to 5): {acidity}, Alcohol (1 to 5):{alcohol}, Tannins (0 to 5):{tannins}, Body (1 to 5): {body}, Wine type (Red, White, Rose, Orange, Sparkling and Fortified):{wine_type}, and a list of flavors: {flavors}. Return a simple list without any introduction or comments.'"
+       
+    return sommelAIer(result)
+
+def sommelAIer(prompt):
+    # Set up your OpenAI API key
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    # Set up the parameters for the GPT request
+    temperature = 0.5
+    max_tokens = 50
+
+    # Send the request to the ChatGPT instance
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=prompt,
+    temperature=temperature,
+    max_tokens=max_tokens,)
+
+    # Print the generated text from the ChatGPT instance
+    return response.choices[0].text.strip()
+    
+if __name__ == '__main__':
+    app.run(debug=True)
